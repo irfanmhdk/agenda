1b39b3da6ddc0861560161d624bbd95d16e380df
fadhilayman <aymanfadhil029@gmail.com>
2023-11-28 13:27:29 +0700

    update isi agenda

--- agenda/isi_agenda.php
+++ agenda/isi_agenda.php
@@ -10,12 +10,53 @@ $proses = mysqli_query($Conn, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda Siswa & Guru</title>
     <link rel="stylesheet" href="navbar.css">
+    <style>
+        input[type=text], select {
+  width: 240px;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=submit] {
+  width: 100px;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+input[type=date] {
+  width: 200px;
+  background-color: #FF0000;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+textarea {
+  width: 240px;
+  height: 150px;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  background-color: #f8f8f8;
+  resize: none;
+}
+        </style>
 </head>
 <body>
     <header>
         <div class="sidebar">
         <a href="beranda.php">Home</a>
-        <a class="active" href="#">Data Agenda</a>
+        <a class="active" href="#"> isi Agenda</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
         </div>
@@ -87,7 +128,7 @@ $proses = mysqli_query($Conn, $sql);
                 <td colspan="3"><textarea name="tujuan_pemb" cols="30" rows="10"></textarea></td>
             </tr>
             <tr>
-                <td colspan="3"></td>
+                <td></td>
                 <td><input type="submit" name="kirim" value="Kirim"></td>
             </tr>
         </table>
