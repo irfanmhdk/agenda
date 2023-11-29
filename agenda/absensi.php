8b5bcb6ad1a41596d1967c46e4f5e0ddf1074151
fadhilayman <aymanfadhil029@gmail.com>
2023-11-29 22:24:04 +0700

    update absensi

--- agenda/absensi.php
+++ agenda/absensi.php
@@ -1,8 +1,8 @@
 <?php 
-    include 'koneksi.php';
+include 'koneksi.php';
 
-    $sql = "SELECT * FROM tb_siswa";
-    $level = mysqli_query($Conn, $sql);
+$sql = "SELECT * FROM tb_siswa";
+$level = mysqli_query($Conn, $sql);
 
 ?>
 <!DOCTYPE html>
@@ -14,82 +14,81 @@
     <title>Absensi Siswa</title>
     <link rel="stylesheet" href="navbar.css">
     <style>
-  table {
-  border-collapse: collapse;
-  width: 100%;
-}
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
 
-th, td {
-  text-align: left;
-  padding: 8px;
-}
+        th, td {
+            text-align: left;
+            padding: 8px;
+        }
 
-tr:nth-child(even){background-color: #f2f2f2}
+        tr:nth-child(even){background-color: #f2f2f2}
 
-th {
-  background-color: #F21818;
-  color: white;
-}
-input[type=submit] {
-  width: 100px;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-</style>
+        th {
+            background-color: #F21818;
+            color: white;
+        }
+
+        input[type=submit] {
+            width: 100px;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+    </style>
+</head>
 <body>
-<header>
+    <header>
         <div class="sidebar">
-        <a href="beranda.php">Home</a>
-        <a class="active" href="#"> isi Agenda</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+            <a href="beranda.php">Home</a>
+            <a class="active" href="#">Isi Agenda</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
         </div>
     </header>
     <div class="content">
-    <center>
-<h1>Data Agenda</h1>
-<br>
-<center>
-<table border="1" cellspacing="0" cellpadding = "10px">
-    <thead>
-        <tr>
-        <th>Nisn</th>
-            <th>Jenis Kelamin</th>
-            <th>Nama</th>
-            <th>Hadir</th>
-            <th>Sakit</th>
-            <th>Izin</th>
-            <th>Alpha</th>
-            </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($level as $row) : ?>
-        <tr>
-                <td><?= $row["nis"];?></td>
-                <td><?= $row["jk"];?></td>
-                <td><?= $row["nama"];?></td>
+        <center>
+            <h1>Data Agenda</h1>
+            <br>
+            <center>
                 <form action="proses_absensi.php" method="post">
-                <td><input type="radio" id="hadir" name="kehadiran<?= $row["nis"];?>" value="Hadir" checked></td>
-                <td><input type="radio" id="sakit" name="kehadiran<?= $row["nis"];?>" value="sakit" ></td>
-                <td><input type="radio" id="izin" name="kehadiran<?= $row["nis"];?>" value="Izin" ></td>
-                <td><input type="radio" id="alpha" name="kehadiran<?= $row["nis"];?>" value="alpha" ></td>
-
-                <?php endforeach ; 
-            ?>
-    </tbody>
-</table>
-<br><br>
-<button type="submit" name="kirim">Submit Absensi</button>
-    </form>
-    </center>
-
-
-</div>
-            
-            
-            
+                    <table border="1" cellspacing="0" cellpadding="10px">
+                        <thead>
+                            <tr>
+                                <th>Nisn</th>
+                                <th>Jenis Kelamin</th>
+                                <th>Nama</th>
+                                <th>Hadir</th>
+                                <th>Sakit</th>
+                                <th>Izin</th>
+                                <th>Alpha</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($level as $row) : ?>
+                                <tr>
+                                    <td><?= $row["nis"];?></td>
+                                    <td><?= $row["jk"];?></td>
+                                    <td><?= $row["nama"];?></td>
+                                    <td><input type="radio" name="kehadiran[<?= $row["nis"];?>]" value="Hadir" checked></td>
+                                    <td><input type="radio" name="kehadiran[<?= $row["nis"];?>]" value="Sakit"></td>
+                                    <td><input type="radio" name="kehadiran[<?= $row["nis"];?>]" value="Izin"></td>
+                                    <td><input type="radio" name="kehadiran[<?= $row["nis"];?>]" value="Alpha"></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                    <br><br>
+                    <button type="submit" name="kirim">Submit Absensi</button>
+                </form>
+            </center>
+        </center>
+    </div>
+</body>
+</html>
--- agenda/proses_absensi.php
+++ agenda/proses_absensi.php
@@ -1,21 +1,31 @@
 <?php
 include("koneksi.php");
 
-// Ambil data dari formulir absensi
-$siswa_id = $_POST['siswa_id'];
-$status_absensi = $_POST['status_absensi'];
-$tanggal = date("Y-m-d");  // Ambil tanggal saat ini
+if (isset($_POST['kirim'])) {
+    $kehadiran = $_POST['kehadiran'];
 
-// Masukkan data absensi ke database
-$query = "INSERT INTO tb_absensi (nis, tanggal, tanggal) VALUES ('$siswa_id', '$status_absensi', '$tanggal')";
-$result = mysqli_query($koneksi, $query);
+    foreach ($kehadiran as $nis => $status_absensi) {
+        $nis = mysqli_real_escape_string($Conn, $nis);
+        $status_absensi = mysqli_real_escape_string($Conn, $status_absensi);
+        $tanggal = date("Y-m-d");
 
-if ($result) {
-    echo "Absensi berhasil dicatat.";
+        $query = "INSERT INTO tb_absen (tanggal, nis, kehadiran) VALUES ('$tanggal', '$nis', '$status_absensi')";
+        $result = mysqli_query($Conn, $query);
+
+        if (!$result) {
+            echo "Gagal mencatat absensi untuk NISN $nis. Error: " . mysqli_error($Conn);
+            break;
+        }
+    }
+
+    if ($result) {
+        echo "Absensi berhasil dicatat.";
+    } else {
+        echo "Gagal mencatat absensi. Error: " . mysqli_error($Conn);
+    }
 } else {
-    echo "Gagal mencatat absensi. Error: " . mysqli_error($koneksi);
+    echo "Tidak ada data yang dikirimkan.";
 }
 
-// Tutup koneksi database
-mysqli_close($koneksi);
+mysqli_close($Conn);
 ?>
