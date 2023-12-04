42114c9cf07c28ae3ea3386ddd8efb8f72001a5b
fadhil ayman <aymanfadhil029@gmail.com>
2023-12-04 09:53:42 +0700

    b

--- agenda/beranda.php
+++ agenda/beranda.php
@@ -80,7 +80,7 @@
         <div class="container">
          <img src="smkn2.jpg" alt="Snow" style="width:100%">
          <a href="tampil_agenda.php"> <button class="btn">Lihat Agenda</button></a>
-         <a href="isi_agenda.php"> <button class="btn1">Isi Agenda</button></a>
+         <a href="data_agenda.php"> <button class="btn1">Isi Agenda</button></a>
          <a href="absensi.php"> <button class="btn2">Isi Absensi</button></a>
         </div>
     </header>
