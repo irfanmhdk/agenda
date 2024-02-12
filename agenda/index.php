3fb1329236ea51a9b63807d2b05d0314a814e98f
Irfan Mahardika <irfanmhdk141@gmail.com>
2024-02-12 10:16:59 +0700

    asd

--- agenda/index.php
+++ agenda/index.php
@@ -20,7 +20,6 @@
     <select class="custom-select" name="role">
       <?php //a
         include 'koneksi.php';
-        session_start();
         $sql = "SELECT * FROM tb_role";
         $proses = mysqli_query($Conn, $sql);
 
