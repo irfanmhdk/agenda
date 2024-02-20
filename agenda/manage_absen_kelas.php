5fcd32f8e62eebf779f3582457d2ba7ad703168d
fadhil <aymanfadhil029.com>
2024-02-20 15:21:23 +0700

    m dan tampil absen

--- agenda/manage_absen_kelas.php
+++ agenda/manage_absen_kelas.php
@@ -144,8 +144,10 @@
     <table>
         <tr>
         <form action="tampil_absen.php" method="POST">
+            
         <div class="select-box">
         <select name="bulan">
+        <option value="Keseluruhan">Keseluruhan</option>
                     <option value="01">Januari</option>
                     <option value="02">Februari</option>
                     <option value="03">Maret</option>
@@ -160,41 +162,17 @@
                     <option value="12">Desember</option>
                 </select>
     </div>
+    <td>
     <div class="select-box">
-        <select name="Tanggal">
-                    <option value="01">01</option>
-                    <option value="02">02</option>
-                    <option value="03">03</option>
-                    <option value="04">04</option>
-                    <option value="05">05</option>
-                    <option value="06">06</option>
-                    <option value="07">07</option>
-                    <option value="08">08</option>
-                    <option value="09">09</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
-                    <option value="31">31</option>
-                </select>
+        <select name="berdasar">
+        <option value="Keseluruhan1">Keseluruhan</option>
+            <option value="Hadir">Hadir</option>
+            <option value="Sakit">Sakit</option>
+            <option value="Izin">Izin</option>
+            <option value="alpha">Alpha</option>
+        </select>
     </div>
+    </td>
         <div class="select-box">
             <?php
              $query = "SELECT * FROM tb_kelas";
@@ -214,7 +192,6 @@
     mysqli_close($Conn);
 ?>    
  </div>
-
 <button class="btn1" name="submit"><i class="fa fa-search"></i></button></form></td>
         </tr>
         <table>
--- agenda/tampil_absen.php
+++ agenda/tampil_absen.php
@@ -4,34 +4,95 @@ session_start();
 
 if (!isset($_SESSION['login'])) {
     exit();
- header("Location: index.php");
-   }
+    header("Location: index.php");
+}
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $tgl = $_POST['bulan'];
-    $tgl1 = $_POST['Tanggal'];
     $search = $_POST['search'];
-    $result = mysqli_query($Conn,  "SELECT 
-    tb_absen.id_absen, 
-    tb_absen.nis, 
-    tb_absen.tanggal, 
-    tb_absen.kehadiran,
-    tb_kelas.nama_kelas,
-    tb_siswa.nama,
-    tb_siswa.jk,
-    tb_siswa.id_kelas
-FROM 
-    tb_absen
-INNER JOIN 
-    tb_siswa ON tb_absen.nis = tb_siswa.nis
-INNER JOIN 
-    tb_kelas ON tb_siswa.id_kelas = tb_kelas.id_kelas 
-WHERE 
-   MONTH  (tb_absen.tanggal) = '$tgl' AND '$tgl1'");
+    $kehadiran = $_POST['berdasar'];
+    if ($tgl == "Keseluruhan" && $kehadiran == "Keseluruhan1") {
+        $result = mysqli_query($Conn,  "SELECT 
+                                            tb_absen.id_absen, 
+                                            tb_absen.nis, 
+                                            tb_absen.tanggal, 
+                                            tb_absen.kehadiran,
+                                            tb_kelas.nama_kelas,
+                                            tb_siswa.nama,
+                                            tb_siswa.jk,
+                                            tb_siswa.id_kelas
+                                        FROM 
+                                            tb_absen
+                                        INNER JOIN 
+                                            tb_siswa ON tb_absen.nis = tb_siswa.nis
+                                        INNER JOIN 
+                                            tb_kelas ON tb_siswa.id_kelas = tb_kelas.id_kelas 
+                                        WHERE 
+                                            tb_siswa.id_kelas = '$search'");
+    } elseif ($tgl == "Keseluruhan") {
+        $result = mysqli_query($Conn,  "SELECT 
+                                            tb_absen.id_absen, 
+                                            tb_absen.nis, 
+                                            tb_absen.tanggal, 
+                                            tb_absen.kehadiran,
+                                            tb_kelas.nama_kelas,
+                                            tb_siswa.nama,
+                                            tb_siswa.jk,
+                                            tb_siswa.id_kelas
+                                        FROM 
+                                            tb_absen
+                                        INNER JOIN 
+                                            tb_siswa ON tb_absen.nis = tb_siswa.nis
+                                        INNER JOIN 
+                                            tb_kelas ON tb_siswa.id_kelas = tb_kelas.id_kelas 
+                                        WHERE 
+                                            tb_siswa.id_kelas = '$search'
+                                            AND tb_absen.kehadiran = '$kehadiran'");
+    } elseif ($kehadiran == "Keseluruhan1") {
+        $result = mysqli_query($Conn,  "SELECT 
+                                            tb_absen.id_absen, 
+                                            tb_absen.nis, 
+                                            tb_absen.tanggal, 
+                                            tb_absen.kehadiran,
+                                            tb_kelas.nama_kelas,
+                                            tb_siswa.nama,
+                                            tb_siswa.jk,
+                                            tb_siswa.id_kelas
+                                        FROM 
+                                            tb_absen
+                                        INNER JOIN 
+                                            tb_siswa ON tb_absen.nis = tb_siswa.nis
+                                        INNER JOIN 
+                                            tb_kelas ON tb_siswa.id_kelas = tb_kelas.id_kelas 
+                                        WHERE 
+                                            tb_siswa.id_kelas = '$search'
+                                            AND  MONTH(tb_absen.tanggal) = '$tgl'");
+    }else{
+        $result = mysqli_query($Conn,  "SELECT 
+                                            tb_absen.id_absen, 
+                                            tb_absen.nis, 
+                                            tb_absen.tanggal, 
+                                            tb_absen.kehadiran,
+                                            tb_kelas.nama_kelas,
+                                            tb_siswa.nama,
+                                            tb_siswa.jk,
+                                            tb_siswa.id_kelas
+                                        FROM 
+                                            tb_absen
+                                        INNER JOIN 
+                                            tb_siswa ON tb_absen.nis = tb_siswa.nis
+                                        INNER JOIN 
+                                            tb_kelas ON tb_siswa.id_kelas = tb_kelas.id_kelas 
+                                        WHERE 
+                                            tb_siswa.id_kelas = '$search'
+                                            AND  MONTH(tb_absen.tanggal) = '$tgl'
+                                            AND tb_absen.kehadiran = '$kehadiran'");
+    }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -39,107 +100,136 @@ WHERE
     <title>Agenda Siswa & Guru</title>
     <link rel="stylesheet" href="navbar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th,
+        td {
+            padding: 8px;
+            color: black;
+            text-align: left;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2
+        }
+
+        .btn {
+            width: auto;
+            background-color: #4CAF50;
+            color: white;
+            padding: 18px 20px;
+            margin: 16px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        .btn:hover {
+            background-color: #3e8e41;
+            color: white;
+        }
+
+        .btn1 {
+            background-color: DodgerBlue;
+            border: none;
+            border-radius: 5px;
+            color: white;
+            padding: 12px 16px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        /* Darker background on mouse-over */
+        .btn1:hover {
+            background-color: RoyalBlue;
+        }
+
+        input[type=text] {
+            width: 240px;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+    </style>
 </head>
-<style>
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
 
-    th, td {
-        padding: 8px;
-        color: black;
-        text-align: left;
-    }
-
-    tr:nth-child(even){background-color: #f2f2f2}
-
-    .btn {
-        width: auto;
-        background-color: #4CAF50;
-        color: white;
-        padding: 18px 20px;
-        margin: 16px 0;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-    }
-
-    .btn:hover {
-        background-color: #3e8e41;
-        color: white;
-    }
-    .btn1 {
-        background-color: DodgerBlue;
-        border: none;
-        border-radius: 5px;
-        color: white;
-        padding: 12px 16px;
-        font-size: 16px;
-        cursor: pointer;
-    }
-
-    /* Darker background on mouse-over */
-    .btn1:hover {
-        background-color: RoyalBlue;
-    }
-    input[type=text] {
-        width: 240px;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        box-sizing: border-box;
-    }
-   
-</style>
 <body>
-<header>
-<div class="sidebar">
-        <a href="beranda3.php"><center><img src="image/2cmi.PNG" style="width: 80px; padding: 5px;"></center></a>
-        <hr  style="width: 90%;">
-        <center><a href="#"><?= date("d F Y"); ?></a></center>
-        <hr  style="width: 90%;">
-        <a href="beranda3.php">Home</a>
-        <a href="data_admin.php">Data Agenda</a>
-        <a href="jadwal.php">Jadwal</a>
-        <button class="dropdown-btn">Manage Data 
-        <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-container">
-            <a href="manage_data_guru.php">Data Guru</a>
-            <a href="manage_data_mapel.php">Data Mata Pelajaran</a>
-            <a href="manage_data_kelas.php">Data Kelas</a>
+    <header>
+        <div class="sidebar">
+            <a href="beranda3.php">
+                <center><img src="image/2cmi.PNG" style="width: 80px; padding: 5px;"></center>
+            </a>
+            <hr style="width: 90%;">
+            <center><a href="#"><?= date("d F Y"); ?></a></center>
+            <hr style="width: 90%;">
+            <a href="beranda3.php">Home</a>
+            <a href="data_admin.php">Data Agenda</a>
+            <a href="jadwal.php">Jadwal</a>
+            <button class="dropdown-btn">Manage Data
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+                <a href="manage_data_guru.php">Data Guru</a>
+                <a href="manage_data_mapel.php">Data Mata Pelajaran</a>
+                <a href="manage_data_kelas.php">Data Kelas</a>
+            </div>
+            <a style="color: red;" href="logout.php"> log out</a>
         </div>
-        <a style="color: red;"href="logout.php"> log out</button></a>
-    </div>
     </header>
     <div class="head" style="display: inline-block;">
         <p style="margin-right: 10px;"><b>Admin</b></p>
     </div>
     <div class="content">
-    <h1>Tampil Absen Kelas</h1><hr>
-    <table>
-         <tr>
-            <th>NIS</th>
-            <th>Nama Siswa</th>
-            <th>Kelas</th>
-            <th>Tanggal</th>
-            <th>kehadiran</th>
-        </tr>
-        <?php
-        foreach($result as $d){ ?>
+        <h1>Tampil Absen Kelas</h1>
+        <hr>
+        <table>
             <tr>
-                <td><?= $d['nis'] ?></td>
-                <td><?= $d['nama'] ?></td>
-                <td><?= $d['id_kelas'] ?></td>
-                <td><?= $d['tanggal'] ?></td>
-                <td><?= $d['kehadiran'] ?></td>
+                <th>NIS</th>
+                <th>Nama Siswa</th>
+                <th>Kelas</th>
+                <th>Tanggal</th>
+                <th>Kehadiran</th>
             </tr>
-        <?php } ?>
-    </table>
+            <?php
+            while ($d = mysqli_fetch_assoc($result)) { ?>
+                <tr>
+                    <td><?= $d['nis'] ?></td>
+                    <td><?= $d['nama'] ?></td>
+                    <td><?= $d['nama_kelas'] ?></td>
+                    <td><?= $d['tanggal'] ?></td>
+                    <td><?= $d['kehadiran'] ?></td>
+                </tr>
+            <?php } ?>
+        </table>
     </div>
     <div class="footer">
         <p>&copy; 2024 By <b>Fadhil</b> & <b>IM</b></p>
     </div>
+
+    <script>
+        /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+        var dropdown = document.getElementsByClassName("dropdown-btn");
+        var i;
+
+        for (i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === "block") {
+                    dropdownContent.style.display = "none";
+                } else {
+                    dropdownContent.style.display = "block";
+                }
+            });
+        }
+    </script>
+</body>
+
+</html>
