dc13f4fa39de48d2b4440c6be2c955057eb72b65
Fadhil <aymanfadhil029@gmail.com>
2024-02-26 09:23:55 +0700

    kepsek

---
+++ agenda/data_user_kepsek.php
@@ -0,0 +1,150 @@
+<?php
+    include 'koneksi.php';
+    session_start();
+ 
+    if (!isset($_SESSION['login'])) {
+        header("Location: index.php");
+        exit();
+    }
+
+    $result = mysqli_query($Conn, "SELECT tb_user.id_user, tb_user.username, tb_user.password, tb_role.nama_role
+                            FROM tb_user INNER JOIN tb_role ON tb_user.role = tb_role.id_role");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda Siswa & Guru</title>
+    <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    th, td {
+        padding: 8px;
+        color: black;
+        text-align: left;
+    }
+
+    tr:nth-child(even){background-color: #f2f2f2}
+
+    .btn {
+        width: auto;
+        background-color: #4CAF50;
+        color: white;
+        padding: 7px 10px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+    .btn:hover {
+        background-color: #3e8e41;
+        color: white;
+    }
+    .btn1 {
+        background-color: DodgerBlue;
+        border: none;
+        border-radius: 5px;
+        color: white;
+        padding: 12px 16px;
+        font-size: 16px;
+        cursor: pointer;
+    }
+
+    /* Darker background on mouse-over */
+    .btn1:hover {
+        background-color: RoyalBlue;
+    }
+    input[type=text] {
+        width: 240px;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+    input[type=submit] {
+        width: 100px;
+        background-color: #4CAF50;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+</style>
+<body>
+<header>
+    <div class="sidebar">
+        <a href="beranda3.php"><center><img src="image/2cmi.PNG" style="width: 80px; padding: 5px;"></center></a>
+        <hr  style="width: 90%;">
+        <center><a href="#"><?= date("d F Y"); ?></a></center>
+        <hr  style="width: 90%;">
+        <a href="kepsek_home.php">Home</a>
+        <a href="report_guru.php">Data Agenda</a>
+        <a class="active" href="data_user_kepsek.php">Manage Data User</a>
+        <a style="color: red;"href="logout.php"> log out</button></a>
+    </div>
+    </header>
+    <div class="head" style="display: inline-block;">
+        <p style="margin-right: 10px;"><b>Admin</b></p>
+    </div>
+    <div class="content">
+    <h1>MANAGE DATA USER</h1><hr>
+    <table>
+        <tr>
+            <td style="text-align:right;"><a href="input_data_user.php"><button class="btn1"><i class="fa fa-plus"></i> Tambah Data</button></a></td>
+        </tr>
+    </table>
+    <table style="box-shadow: 7px 7px 5px lightgrey;">
+        <tr>
+            <th>Username</th>
+            <th>Password</th>
+            <th>Role</th>
+            <th>Opsi</th>
+        </tr>
+        <?php
+        foreach($result as $d){ ?>
+            <tr>
+                <td><?= $d['username'] ?></td>
+                <td><?= $d['password'] ?></td>
+                <td><?= $d['nama_role'] ?></td>
+                <td><a href="edit_data_user.php?id=<?= $d['id_user'] ?>"><button class="btn1" name="submit" style="font-size: 11px;background-color: #ffcc00;color: #000000;"><i class="fa fa-edit"> EDIT</i></button></a>
+                <td><a href="hapus_data_user.php?id=<?= $d['id_user'] ?>"><button class="btn1" onclick="return confirm('Anda Akan Menghapus Data, Yakin?')" name="submit" style="font-size: 11px; background-color: #cc3300;"><i class="fa fa-trash"> HAPUS</i></button></form></td>
+            </tr>
+        <?php } ?>
+    </table>
+    </div>
+    <div class="footer">
+        <p>&copy; 2024 By <b>Fadhil</b> & <b>IM</b></p>
+    </div>
+
+    <script>
+    /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+
+    for (i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+          dropdownContent.style.display = "block";
+    }
+      });
+    }
+  </script>
+</body>
+</html>
