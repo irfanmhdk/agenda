53cec2c6965a034afb42aa3140b6966aba854aa9
Irfan Mahardika <irfanmhdk141@gmail.com>
2024-02-26 09:40:47 +0700

    kegiatan lain

--- agenda/input_data_user.php
+++ agenda/input_data_user.php
@@ -64,6 +64,7 @@
             <hr  style="width: 90%;">
             <a href="beranda3.php">Home</a>
             <a href="data_admin.php">Data Agenda</a>
+            <a href="kegiatan_admin.php">Kegiatan Lainnya</a>
             <a href="jadwal.php">Jadwal</a>
             <a class="active" href="manage_data_user.php">Manage Data User</a>
             <button class="dropdown-btn">Manage Data 
