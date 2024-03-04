78fe049e18d542b698674b9bf58713121b6c9c21
Irfan Mahardika <irfanmhdk141@gmail.com>
2024-03-04 08:42:57 +0700

    nav

--- agenda/comment.php
+++ agenda/comment.php
@@ -26,6 +26,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comment Guru</title>
     <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <style>
     table {
@@ -79,7 +80,13 @@
             <center><a href="#"><?= date("d F Y"); ?></a></center>
             <hr  style="width: 90%;">
             <a href="beranda2.php?id=<?= $nip ?>">Home</a>
-            <a class="active" href="verifikasi.php?id=<?= $nip ?>">Verifikasi</a>
+            <button class="dropdown-btn">Verifikasi Agenda
+            <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+                <a href="verifikasi.php?id=<?= $nip ?>">Agenda Hari Ini</a>
+                <a href="verifikasi_semua.php?id=<?= $nip ?>">Agenda</a>
+            </div>
             <a style="color: red;"href="logout.php"> log out</button></a>
         </div>
     </header>
@@ -116,5 +123,22 @@
             <td><input type="submit" value="Kirim Komentar" class="btn">
         </form>
     </div>
+    <script>
+    /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+
+    for (i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+          dropdownContent.style.display = "block";
+    }
+      });
+    }
+    </script>
 </body>
 </html>
