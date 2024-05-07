2f31c394f2ae595c7238c3c899e41781be1b835f
Irfan Mahardika <irfanmhdk141@gmail.com>
2024-05-07 08:57:46 +0700

    script

--- agenda/beranda.php
+++ agenda/beranda.php
@@ -156,17 +156,6 @@
   </form>
 </div>
 
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
         </div>
     <div class="footer">
         <p>&copy; 2024 By <b>Fadhil</b> & <b>IM</b></p>
